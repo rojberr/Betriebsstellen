c0623590e7b88d9e2686813b5c807c21f83bd37a
Rojberr <rojberr@outlook.com>
2021-05-22 23:55:14 +0200

    init commit

---
+++ src/main/java/dev/drzymala/betriebsstellen/BetriebsstellenApplication.java
@@ -0,0 +1,13 @@
+package dev.drzymala.betriebsstellen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetriebsstellenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetriebsstellenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/drzymala/betriebsstellen/BetriebsstellenApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.drzymala.betriebsstellen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetriebsstellenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
