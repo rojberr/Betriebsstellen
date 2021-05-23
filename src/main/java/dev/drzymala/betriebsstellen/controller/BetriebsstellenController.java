4394459781663f72aa78c4fe4162e1ae1c0922f7
Rojberr <rojberr@outlook.com>
2021-05-23 12:04:57 +0200

    controller uploadCSVFile

--- src/main/java/dev/drzymala/betriebsstellen/controller/BetriebsstellenController.java
+++ src/main/java/dev/drzymala/betriebsstellen/controller/BetriebsstellenController.java
@@ -1,10 +1,23 @@
 package dev.drzymala.betriebsstellen.controller;
 
 
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+import dev.drzymala.betriebsstellen.entity.Betriebsstelle;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
-@RestController
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.List;
+
+@Controller
 public class BetriebsstellenController {
 
     @GetMapping("/")
@@ -12,4 +25,40 @@ public class BetriebsstellenController {
         return "index";
     }
 
+
+    @PostMapping("/upload-csv-file")
+    public String uploadCSVFile(@RequestParam("file") MultipartFile file, Model model) {
+
+        // validate file
+        if (file.isEmpty()) {
+            model.addAttribute("message", "Please select a CSV file to upload.");
+            model.addAttribute("status", false);
+        } else {
+
+            // parse CSV file to create a list of `User` objects
+            try (Reader reader = new BufferedReader(new InputStreamReader(file.getInputStream()))) {
+
+                // create csv bean reader
+                CsvToBean<Betriebsstelle> csvToBean = new CsvToBeanBuilder(reader)
+                        .withType(Betriebsstelle.class)
+                        .withIgnoreLeadingWhiteSpace(true)
+                        .build();
+
+                // convert `CsvToBean` object to list of users
+                List<Betriebsstelle> betriebsstellen = csvToBean.parse();
+
+                // TODO: save betriebsstellen in DB
+
+                // save users list on model
+                model.addAttribute("betriebsstelle", betriebsstellen);
+                model.addAttribute("status", true);
+
+            } catch (Exception ex) {
+                model.addAttribute("message", "An error occurred while processing the CSV file.");
+                model.addAttribute("status", false);
+            }
+        }
+
+        return "file-upload-status";
+    }
 }
